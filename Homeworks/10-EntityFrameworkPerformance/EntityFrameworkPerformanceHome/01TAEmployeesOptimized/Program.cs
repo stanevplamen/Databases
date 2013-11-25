1a4e1e666ae57ad12d257149bfb696b455d9f59c
stanevplamen <stanev.plamen@gmail.com>
2013-11-25 16:35:26 +0200

    added some EF performance tasks

---
+++ Homeworks/10-EntityFrameworkPerformance/EntityFrameworkPerformanceHome/01TAEmployeesOptimized/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using TelerikAcademyModel;
+
+namespace TAEmployees
+{
+    class Program
+    {
+        static void Main()
+        {
+            GetEmployeesFast();
+            GetEmployeesSlow();
+        }
+
+        private static void GetEmployeesSlow()
+        {
+            TelerikAcademyEntities telerikEntity = new TelerikAcademyEntities();
+
+            foreach (var employee in telerikEntity.Employees)
+            {
+                Console.WriteLine("{0} {1}", employee.FirstName, employee.LastName);
+                Console.WriteLine("{0}", employee.Departments1.Name);
+                Console.WriteLine("{0}", employee.Addresses.Towns.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine(telerikEntity.Employees.Count());
+        }
+
+        private static void GetEmployeesFast()
+        {
+            TelerikAcademyEntities telerikEntity = new TelerikAcademyEntities();
+
+            foreach (var employee in telerikEntity.Employees.Include("Addresses.Towns").Include("Departments"))
+            {
+                Console.WriteLine("{0} {1}", employee.FirstName, employee.LastName);
+                Console.WriteLine("{0}", employee.Departments1.Name);
+                Console.WriteLine("{0}", employee.Addresses.Towns.Name);
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homeworks/10-EntityFrameworkPerformance/EntityFrameworkPerformanceHome/02TAList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelerikAcademyModel;
+
+namespace TAList
+{
+    class Program
+    {
+        public static void Main()
+        {
+            SelectWithMultipleToList(); // Produces 645 queries.
+            SelectWithOptimizedToList(); // Produces only one query.
+        }
+
+        private static void SelectWithMultipleToList()
+        {
+            using (var telerikAcademyDBContext = new TelerikAcademyEntities())
+            {
+                var result = telerikAcademyDBContext.Employees.ToList()
+                    .Select(e => e.Addresses).ToList().Select(a => a.Towns).ToList()
+                    .Where(t => t.Name == "Sofia").ToList();
+            }
+            Console.WriteLine("Method 1 finished");
+        }
+
+        private static void SelectWithOptimizedToList()
+        {
+            using (var telerikAcademyDBContext = new TelerikAcademyEntities())
+            {
+                var result = telerikAcademyDBContext.Employees
+                    .Select(e => e.Addresses).Select(a => a.Towns)
+                    .Where(t => t.Name == "Sofia").ToList();
+            }
+            Console.WriteLine("Method 2 finished");
+        }
+    }
+}
